c403a91f475ae86d172ad58775976907ed21750d
robbiemie <yang657850144@163.com>
2026-07-28 17:24:12 +0800

    chore: bump version to 0.0.8 and rebuild dist

--- html/iframePage/dist/static/js/javascript-b070306a.js
+++ html/iframePage/dist/static/js/javascript-b070306a.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-1220ad1e.js";import"./index-1d7fbb5c.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-4dbcb74e.js";import"./index-ca7937e2.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.34.1(547870b6881302c5b4ff32173c16d06009e3588f)
  * Released under the MIT license
